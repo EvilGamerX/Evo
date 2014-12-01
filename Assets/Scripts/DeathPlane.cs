17a69407dff2f4a3c379ecfbbc1327077bf4d406
EvilGamerX <evilgamerx42@gmail.com>
2014-11-30 21:09:06 -0500

    Setting up basic flatworld with a DeathPlane

---
+++ Assets/Scripts/DeathPlane.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathPlane : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider obj)
+    {
+        obj.transform.position.Set(0f, 1f, 0f);
+    }
+}
